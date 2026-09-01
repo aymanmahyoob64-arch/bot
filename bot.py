6337753f4237149e76b08f503e8f8f3a3dce9709
aymanmahyoob64-arch <aymanmahyoob64@gmail.com>
2026-09-01 07:34:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
 import os
+import threading
+from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+# سيرفر وهمي حتى Render يرضى
+app_flask = Flask(__name__)
+@app_flask.route('/')
+def home():
+    return "Bot is running"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app_flask.run(host='0.0.0.0', port=port)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ البوت شغال تمام! هلا بيك - بوت كنان")
 
 def main():
+    # شغل السيرفر الوهمي
+    threading.Thread(target=run_flask, daemon=True).start()
+    
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     print("Bot is running...")
