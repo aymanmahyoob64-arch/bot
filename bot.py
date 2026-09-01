222eaf5a6e70681f6b55b7e2b623b494599b458e
aymanmahyoob64-arch <aymanmahyoob64@gmail.com>
2026-09-01 06:30:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
 import os, threading
 from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home(): return "Bot is running - Kanan"
-def run_flask():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-threading.Thread(target=run_flask, daemon=True).start()
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+
+# Web server for Render Free plan
+flask_app = Flask(__name__)
+@flask_app.route('/')
+def home(): return "Bot is running - Kanan"
+def run_flask():
+    flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+threading.Thread(target=run_flask, daemon=True).start()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت شغال ✅")
 
 if __name__ == "__main__":
     print("Bot started")
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    bot_app = ApplicationBuilder().token(TOKEN).build()
+    bot_app.add_handler(CommandHandler("start", start))
+    bot_app.run_polling()
