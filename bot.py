e7b4fe43fb052af8c74e0759aba8137865dbc878
aymanmahyoob64-arch <aymanmahyoob64@gmail.com>
2026-09-01 05:42:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,14 @@
 import os
-import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-logging.basicConfig(level=logging.INFO)
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("البوت شغال ✅\nارسل لي اي شي")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"استلمت: {update.message.text}")
+    await update.message.reply_text("البوت شغال ✅")
 
 if __name__ == "__main__":
     print("Bot started")
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     app.run_polling()
